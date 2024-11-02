daaeee9e6cb1122e1063d8ecd90b5dd21f153f45
Ayush Kumar <7126924ak@gmail.com>
2024-11-02 23:47:00 +0530

    add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import { Hero } from './components/Hero'
 
 function App() {
 
   return (
     <div className='text-blueish font-open mx-auto'>
-     
+      <Hero/>
+      
     </div>
   )
 }
