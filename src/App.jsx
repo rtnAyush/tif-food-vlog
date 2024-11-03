d663ef94d8fa99dd848637268810c1c8a1eedd19
Ayush Kumar <7126924ak@gmail.com>
2024-11-03 13:34:00 +0530

    add Article section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Hero } from './components/Hero'
 import { About } from './components/About'
+import { ArticleCards } from './components/ArticleCards'
 
 
 function App() {
 
   return (
     <div className='text-blueish font-open mx-auto'>
-      <Hero/>
-      <About/>
+      <Hero />
+      <About />
+      <ArticleCards />
     </div>
   )
 }
