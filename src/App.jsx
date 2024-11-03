36d9f1bb94c029ddf2131b028b1bbab60b2281a9
Ayush Kumar <7126924ak@gmail.com>
2024-11-03 10:47:00 +0530

    add About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Hero } from './components/Hero'
+import { About } from './components/About'
+
 
 function App() {
 
   return (
     <div className='text-blueish font-open mx-auto'>
       <Hero/>
-      
+      <About/>
     </div>
   )
 }
