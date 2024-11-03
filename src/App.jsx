36f76bd63cc7e20dfc23735aecc9cb7fa2b41b6c
Ayush Kumar <7126924ak@gmail.com>
2024-11-03 14:02:00 +0530

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Hero } from './components/Hero'
 import { About } from './components/About'
 import { ArticleCards } from './components/ArticleCards'
+import { Footer } from './components/Footer'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <ArticleCards />
+      <Footer />
     </div>
   )
 }
